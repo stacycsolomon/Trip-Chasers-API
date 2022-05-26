4ef75d33daf998476b79a7f468cfd08f2ba4dc97
ssolomon <stacycsolomon@gmail.com>
2022-05-26 17:51:47 -0400

    Modified

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'Trip-Chasers-API'
+const mongooseBaseName = 'trip-chasers-api'
 
 // create the mongodb uri for development and test
 const database = {
